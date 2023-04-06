8a78d2fa786ade0fd6ab27ad7d441ac169ef5827
Tan Çetiner <cetinertan13@gmail.com>
2023-04-06 10:12:14 +0300

    updates on frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,86 +1,93 @@
-import React, {useState, useEffect} from "react";
-import {IconMenu2, IconX} from "@tabler/icons";
-import {WalletConnect} from "@/helpers/UserHelper";
-import Link from "next/link";
+import React, { useState, useEffect } from "react"
+import { IconMenu2, IconX } from "@tabler/icons"
+import { WalletConnect } from "@/helpers/UserHelper"
+import Link from "next/link"
 
-function Header({logged}) {
-    const [hamburger, setHamburger] = useState(false);
+function Header({ logged }) {
+  const [hamburger, setHamburger] = useState(false)
 
-    const handleHamburger = () => {
-        setHamburger(!hamburger);
-    };
+  const handleHamburger = () => {
+    setHamburger(!hamburger)
+  }
 
-
-    return (
-        <>
-            <div className="Header-container"></div>
-            <div className="Header">
-                <div className="Header-web">
-                    <Link href="/" className="Header-logo">
-                        SUGovern
-                    </Link>
-                    <div className="Header-links">
-                        <Link href="/projects" className="Header-link">
-                            Projects
-                        </Link>
-                        <Link className="Header-link" href="/docs">
-                            Wiki
-                        </Link>
-                    </div>
-                    {logged ? (
-                        <div className="Header-link Wallet">Connected</div>
-                        // TODO: Disconnect ekle ve ikonlastir
-                    ) : (
-                        <div className="Wallet" style={{cursor: "pointer"}} onClick={WalletConnect}>Wallet Connect</div>
-                    )}
-                </div>
-
-                {hamburger ? (
-                    <div className="Header-mobile hamburger-active">
-                        <div className="Header-mobile-top">
-                            <Link  href="/" className="Header-logo">
-                                SUGovern
-                            </Link >
-                            <div className="Header-hamburger" onClick={handleHamburger}>
-                                {hamburger ? (
-                                    <IconX width={32} height={32}/>
-                                ) : (
-                                    <IconMenu2 width={32} height={32}/>
-                                )}
-                            </div>
-                        </div>
-                        <div className="Header-links-mobile">
-                            <div className="Header-links-mobile-links">
-                                <a href="/projects" className="Header-link-mobile">
-                                    Projects
-                                </a>
-                            </div>
-                            {logged ? (
-                                <div className="Wallet">Connected</div>
-                            ) : (
-                                <div className="Wallet" onClick={WalletConnect}>Wallet Connect</div>
-                            )}
-                        </div>
-                    </div>
-                ) : (
-                    <div className="Header-mobile">
-                        <div className="Header-mobile-top">
-                            <Link  href="/" className="Header-logo">
-                                SUGovern
-                            </Link >
-                            <div className="Header-hamburger" onClick={handleHamburger}>
-                                {hamburger ? (
-                                    <IconX width={32} height={32}/>
-                                ) : (
-                                    <IconMenu2 width={32} height={32}/>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                )}
+  return (
+    <>
+      <div className="Header-container"></div>
+      <div className="Header">
+        <div className="Header-web">
+          <Link href="/" className="Header-logo">
+            SUGovern
+          </Link>
+          <div className="Header-links">
+            <Link href="/projects" className="Header-link">
+              Projects
+            </Link>
+            <Link className="Header-link" href="/docs">
+              Wiki
+            </Link>
+          </div>
+          {logged ? (
+            <div className="Header-link Wallet"> {logged} </div>
+          ) : (
+            // TODO: Disconnect ekle ve ikonlastir
+            <div
+              className="Wallet"
+              style={{ cursor: "pointer" }}
+              onClick={WalletConnect}
+            >
+              Wallet Connect
             </div>
-        </>
-    );
+          )}
+        </div>
+
+        {hamburger ? (
+          <div className="Header-mobile hamburger-active">
+            <div className="Header-mobile-top">
+              <Link href="/" className="Header-logo">
+                SUGovern
+              </Link>
+              <div className="Header-hamburger" onClick={handleHamburger}>
+                {hamburger ? (
+                  <IconX width={32} height={32} />
+                ) : (
+                  <IconMenu2 width={32} height={32} />
+                )}
+              </div>
+            </div>
+            <div className="Header-links-mobile">
+              <div className="Header-links-mobile-links">
+                <a href="/projects" className="Header-link-mobile">
+                  Projects
+                </a>
+              </div>
+              {logged ? (
+                <div className="Wallet">Connected</div>
+              ) : (
+                <div className="Wallet" onClick={WalletConnect}>
+                  Wallet Connect
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="Header-mobile">
+            <div className="Header-mobile-top">
+              <Link href="/" className="Header-logo">
+                SUGovern
+              </Link>
+              <div className="Header-hamburger" onClick={handleHamburger}>
+                {hamburger ? (
+                  <IconX width={32} height={32} />
+                ) : (
+                  <IconMenu2 width={32} height={32} />
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
 
-export default Header;
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,94 +1,97 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head"
+import Image from "next/image"
+import styles from "@/styles/Home.module.css"
 import {
-    BindContract,
-    WalletConnect,
-    DaoIsExist,
-    fetchNextDaoId,
-    fetchAllDaos,
-    NetworkControl
-} from '@/helpers/UserHelper'
-import {useEffect, useState} from 'react'
-import Header from "@/components/Header";
-import Card from "@/components/Card";
-import {SimpleGrid, Spinner} from "@chakra-ui/react";
-import Popup from "@/components/Popup";
-import Web3 from "web3";
-import {DAO_ADDRESS, DAO_JSON, FACTORY_JSON, CHAIN_ID, TOKEN_ADDRESS} from "../../constant";
+  BindContract,
+  WalletConnect,
+  DaoIsExist,
+  fetchNextDaoId,
+  fetchAllDaos,
+  NetworkControl,
+} from "@/helpers/UserHelper"
+import { useEffect, useState } from "react"
+import Header from "@/components/Header"
+import Card from "@/components/Card"
+import { SimpleGrid, Spinner } from "@chakra-ui/react"
+import Popup from "@/components/Popup"
+import Web3 from "web3"
+import {
+  DAO_ADDRESS,
+  DAO_JSON,
+  FACTORY_JSON,
+  CHAIN_ID,
+  TOKEN_ADDRESS,
+} from "../../constant"
 
 export default function Home() {
-    const [account, setAccount] = useState(null);
-    const [alertMessage, setAlertMessage] = useState({text: "", title: ""}); //this is used inside Popup component, to pass a message to the inside of the popup when an error occurs, or a transaction is successful, or in a case of warning
-    const [popupTrigger, setPopupTrigger] = useState(false);  //this is used inside Popup component, to trigger the popup to show up
-    const [daoFactoryContract, setDaoFactoryContract] = useState(undefined); //to store the DAOFactory contract instance
-    const [all_daos, setall_daos] = useState([]); //to store all the DAOs created by the DAOFactory contract
-    const [topDAOAddress, setTopDAOAddress] = useState(""); //to store the address of the top DAO
-    const [isCorrect, setIsCorrect] = useState(false); //this is used to change between the tabs, we will set it when a user clicks on the buttons on the sidebar, in default it is set to 10, which is the view proposals tab
-    const [inp, setInp] = useState('https://avatars.githubusercontent.com/u/48164025?v=4'); //this is used to change between the tabs, we will set it when a user clicks on the buttons on the sidebar, in default it is set to 10, which is the view proposals tab
-    const [loaded, setLoaded] = useState(false); //to check if the page is loaded, i.e. all the DAOs are fetched from the blockchain
+  const [account, setAccount] = useState(null)
+  const [alertMessage, setAlertMessage] = useState({ text: "", title: "" }) //this is used inside Popup component, to pass a message to the inside of the popup when an error occurs, or a transaction is successful, or in a case of warning
+  const [popupTrigger, setPopupTrigger] = useState(false) //this is used inside Popup component, to trigger the popup to show up
+  const [daoFactoryContract, setDaoFactoryContract] = useState(undefined) //to store the DAOFactory contract instance
+  const [all_daos, setall_daos] = useState([]) //to store all the DAOs created by the DAOFactory contract
+  const [topDAOAddress, setTopDAOAddress] = useState("") //to store the address of the top DAO
+  const [isCorrect, setIsCorrect] = useState(false) //this is used to change between the tabs, we will set it when a user clicks on the buttons on the sidebar, in default it is set to 10, which is the view proposals tab
+  const [inp, setInp] = useState(
+    "https://avatars.githubusercontent.com/u/48164025?v=4"
+  ) //this is used to change between the tabs, we will set it when a user clicks on the buttons on the sidebar, in default it is set to 10, which is the view proposals tab
+  const [loaded, setLoaded] = useState(false) //to check if the page is loaded, i.e. all the DAOs are fetched from the blockchain
 
-    useEffect(() => {
-            if (!loaded) {
-                WalletConnect().then((res) => {
-                    setAccount(res);
-                });
-                if (!daoFactoryContract) {
-                    setDaoFactoryContract(BindContract(FACTORY_JSON["abi"], DAO_ADDRESS));
-                } else {
-                    let res;
-                    if (all_daos.length === 0) {
-                        fetchAllDaos(daoFactoryContract).then((result) => setall_daos(result))
-                    } else {
-                        setLoaded(true)
-                    }
-                }
-            }
-
+  useEffect(() => {
+    if (!loaded) {
+      WalletConnect().then((res) => {
+        setAccount(res)
+      })
+      if (!daoFactoryContract) {
+        setDaoFactoryContract(BindContract(FACTORY_JSON["abi"], DAO_ADDRESS))
+      } else {
+        let res
+        if (all_daos.length === 0) {
+          fetchAllDaos(daoFactoryContract).then((result) => setall_daos(result))
+        } else {
+          setLoaded(true)
         }
-        ,
-        [daoFactoryContract, all_daos, loaded]
-    )
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(e.target[0].value);
-        setInp((e.target[0].value))
+      }
     }
+  }, [daoFactoryContract, all_daos, loaded])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target[0].value)
+    setInp(e.target[0].value)
+  }
 
-    return (
-        <div>
-            <Head>
-                <title>DAO APP</title>
-                <meta name="description" content="A blockchain dao app"/>
-            </Head>
-            <Header logged={account}/>
-            <div className="index-page">
-
-                {!loaded ? (
-                    <Spinner></Spinner>
-                ) : (
-                    <SimpleGrid columns={1} spacing="40px">
-                        {all_daos.map((dao, index) => {
-                            return (
-                                <Card
-                                    index={index}
-                                    key={index}
-                                    address={`/dao?address=${dao[0]}`}
-                                    title={String(dao[1]).concat(
-                                        topDAOAddress === dao[0] ? " (top dao)" : ""
-                                    )}
-                                    text={dao[2]}
-                                />
-                            );
-                        })}
-                    </SimpleGrid>
-                )}
-            </div>
-            <Popup trigger={popupTrigger} setTrigger={setPopupTrigger}>
-                <h2 className="h2 text-black">{alertMessage.title}</h2>
-                <p>{alertMessage.text}</p>
-            </Popup>
-        </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>DAO APP</title>
+        <meta name="description" content="A blockchain dao app" />
+      </Head>
+      <Header logged={account} />
+      <div className="index-page">
+        {!loaded ? (
+          <Spinner></Spinner>
+        ) : (
+          <SimpleGrid columns={1} spacing="40px">
+            {all_daos.map((dao, index) => {
+              return (
+                <Card
+                  index={index}
+                  key={index}
+                  address={`/dao?address=${dao[0]}`}
+                  title={String(dao[1]).concat(
+                    topDAOAddress === dao[0] ? " (top dao)" : ""
+                  )}
+                  text={dao[2]}
+                />
+              )
+            })}
+          </SimpleGrid>
+        )}
+      </div>
+      <Popup trigger={popupTrigger} setTrigger={setPopupTrigger}>
+        <h2 className="h2 text-black">{alertMessage.title}</h2>
+        <p>{alertMessage.text}</p>
+      </Popup>
+    </div>
+  )
 }
