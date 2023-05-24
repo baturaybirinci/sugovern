e0f4a297de95842da46e77ff799b5b95e140bb40
Tan Çetiner <cetinertan13@gmail.com>
2023-05-24 17:06:54 +0300

    fixed dao image aspect ratio

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,25 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState } from "react"
 
-function Card({address,index,title,text,imageUrl}) {
-    return (
-        <a href={address}>
-            {/* className="item-card-link"  */}
-            <div
-                key={index}
-                style={{ cursor: "pointer" }}
-            >
-                <div className="item-card">
-                    <div>
-                        <img
-                            src={imageUrl}
-                            alt=""
-                            style={{ width: "100%", height: "25vh", objectFit: "fill" }}
-                        />
-                    </div>
-                    <div className="card-text">
-                        <h3 className="card-title">{title}</h3>
-                        <p className="card-desc">{text}</p>
-                    </div>
-                </div>
-            </div>
-        </a>
-    );
+function Card({ address, index, title, text, imageUrl }) {
+  return (
+    <a href={address}>
+      {/* className="item-card-link"  */}
+      <div key={index} style={{ cursor: "pointer" }}>
+        <div className="item-card">
+          <div>
+            <img
+              src={imageUrl}
+              alt=""
+              style={{ width: "100%", height: "%100", objectFit: "fill" }}
+            />
+          </div>
+          <div className="card-text">
+            <h3 className="card-title">{title}</h3>
+            <p className="card-desc">{text}</p>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
 }
-export default Card;
+export default Card
